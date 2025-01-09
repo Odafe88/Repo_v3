85821d14bcd9b372f534d1641871207c0bdc405a
Odafe Alaiya <76574625+Odafe88@users.noreply.github.com>
2025-01-09 01:11:41 +0100

    Update: Fixed bug related to contact form

--- dafe-portfolio/src/components/Contact.jsx
+++ dafe-portfolio/src/components/Contact.jsx
@@ -1,28 +1,75 @@
+"use client"
 import { useState, useRef } from "react";
 import emailjs from '@emailjs/browser';
+
 import { motion } from "framer-motion";
 
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 const Contact = () => {
-    const form = useRef();
 
-    const sendEmail = (e) => {
-        e.preventDefault();
+  const [userInput, setUserInput] = useState({
+    name: "",
+    email: "",
+    message: ""
+  });
 
-        emailjs
-        .sendForm(process.env.SERVICE_ID, process.env.TEMPLATE_ID, form.current, {
-            publicKey: 'YOUR_PUBLIC_KEY',
-        })
-        .then(
-            () => {
-            console.log('SUCCESS!');
-            },
-            (error) => {
-            console.log('FAILED...', error.text);
-            },
-        );
-    };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserInput({
+      ...userInput,
+      [name]: value
+    });
+  };
+    
+
+  const sendEmail = async (e) => {
+    e.preventDefault();
+
+    const serviceID = process.env.SERVICE_ID;
+    const templateID = process.env.TEMPLATE_ID;
+    const userID = process.env.PUBLLIC_KEY;
+
+    try {
+      const emailParams = {
+        name: userInput.name,
+        email: userInput.email,
+        message: userInput.message
+      };
+
+      const res = await emailjs.send(serviceID, templateID, emailParams, userID);
+
+      if (res.status === 200) {
+        toast.success("Message sent successfully!", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        });
+        setUserInput({
+          name: "",
+          email: "",
+          message: ""
+        });
+      }
+    } catch (error) {
+      toast.error("Failed to send message. Please try again later.", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+      });
+      console.error("Error:", error)
+    }
+  }
 
     return (
+      <>
         <motion.div 
         initial={{opacity: 0, y: 100}}
         animate={{
@@ -34,35 +81,60 @@ const Contact = () => {
       >
         <div className="w-full md:w-[70%] mx-auto">
           <h2 className="text-[40px] md:text-[60px] text-[#e1e1e2] font-semibold mb-8">Get In Touch</h2>
-          <form ref={form} onSubmit={sendEmail} className="space-y-6">
+          <form onSubmit={sendEmail} className="space-y-6">
             <div className="flex flex-col md:flex-row gap-4">
               <input 
-                type="text" 
-                placeholder="Name" 
-                className="flex-1 bg-transparent border border-[#333] rounded-md p-4 text-[#e1e1e2] focus:border-none transition-colors"
+                type="text"
+                name="name" 
+                placeholder="Name"
+                value={userInput.name}
+                onChange={handleChange}
+                className="flex-1 bg-transparent border border-[#333] rounded-md p-4 text-[#e1e1e2] focus:text-[#e1e1e2] focus:border-none focus:bg-transparent transition-colors"
               />
               <input 
                 type="email" 
-                placeholder="Email" 
-                className="flex-1 bg-transparent border border-[#333] rounded-md p-4 text-[#e1e1e2] focus:border-none transition-colors"
+                name="email"
+                placeholder="Email"
+                value={userInput.email}
+                onChange={handleChange}
+                className="flex-1 bg-transparent border border-[#333] rounded-md p-4 text-[#e1e1e2] focus:text-[#e1e1e2] focus:bg-transparent focus:border-none transition-colors"
               />
             </div>
             
             <textarea 
               placeholder="Message" 
-              rows="6" 
+              rows="6"
+              name="message"
+              value={userInput.message}
+              onChange={handleChange}
               className="w-full bg-transparent border border-[#333] rounded-md p-4 text-[#e1e1e2] focus:border-none transition-colors"
             ></textarea>
             
             <button 
               type="submit"
-              className="px-8 py-4 border border-[#e1e1e2] rounded-md text-[#e1e1e2] hover:text-[#333] hover:border-[#333] transition-colors"
+              className="px-8 py-4 border border-[#e1e1e2] rounded-md text-[#e1e1e2] focus:text-[#e1e1e2] focus:border-none focus:bg-transparent hover:text-[#333] hover:border-[#333] transition-colors"
             >
               Send Message
             </button>
           </form>
         </div>
       </motion.div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        closeOnClick={true}
+        pauseOnHover={true}
+        draggable={true}
+        theme="dark"
+        toastClassName="font-noto"
+        style={{
+          '--toastify-color-dark': '#121112',
+          '--toastify-text-color-dark': '#e1e1e2',
+          '--toastify-color-progress-dark': '#333'
+        }}
+      />
+      </>
     );
 };
 
--- dafe-portfolio/src/components/Footer.jsx
+++ dafe-portfolio/src/components/Footer.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 
 import { FaGithub, FaX, FaLinkedin } from "react-icons/fa6";
